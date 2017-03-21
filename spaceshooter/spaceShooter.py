9f427d8db31a1230b5bae289929b1d729e76816c
joetechem <joe@techemstudios.com>
2017-03-21 12:55:02 -0400

    removing and adding bugs

--- spaceshooter/spaceShooter.py
+++ spaceshooter/spaceShooter.py
@@ -12,18 +12,18 @@ sound_folder = path.join(path.dirname(__file__), 'sounds')
 
 WIDTH = 480
 HEIGHT = 600
-FPS = 60
+FPS = 20
 POWERUP_TIME = 5000
 BAR_LENGTH = 100
 BAR_HEIGHT = 10
 
 # Define Colors 
-WHITE = (255, 255, )
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-YELLOW = (255, 255, 0)
+##WHITE = (255, 255, )
+##BLACK = (0, 0, 0)
+##RED = (255, 0, 0)
+##GREEN = (0, 255, 0)
+##BLUE = (0, 0, 255)
+##YELLOW = (255, 255, 0)
 
 pygame.init()
 pygame.mixer.init()  
@@ -162,7 +162,7 @@ class Player(pygame.sprite.Sprite):
 
 
 
-                keystate = pygame.key.get_pressed()     
+        keystate = pygame.key.get_pressed()     
         if keystate[pygame.K_LEFT]:
             self.speedx = -5
         elif keystate[pygame.K_RIGHT]:
